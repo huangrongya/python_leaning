63bfa64394949e210df55973649fb19a76180b2b
黄荣亚 <19area@.163.com>
2018-06-06 17:42:45 +0800

    /

---
+++ generator/demo_2.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# __author : huangrongya
+# @Desc : ================================================
+# Life is short I use Python!!!                        ===
+# If this runs wrong,don't ask me,I don't know why...  ===
+# If this runs right,thank god,and I don't know why... ===
+# Maybe the answer,my friend,is blowing in the wind... ===
+# ========================================================
+# date : 2018/6/6 0006 15:02
+# @Project : python_leaning
+# @FileName: demo_2.py
---
+++ iteration/demo_1.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# __author : huangrongya
+# @Desc : ================================================
+# Life is short I use Python!!!                        ===
+# If this runs wrong,don't ask me,I don't know why...  ===
+# If this runs right,thank god,and I don't know why... ===
+# Maybe the answer,my friend,is blowing in the wind... ===
+# ========================================================
+# date : 2018/6/6 0006 10:41
+# @Project : python_leaning
+# @FileName: demo_1.py
